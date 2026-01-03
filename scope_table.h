ad1f4c7feab26726eefea323f1d04b38084589db
Abrar Shahrier Arnab <120324766+arnob56@users.noreply.github.com>
2026-01-04 00:15:47 +0600

    Update scope_table.h

--- scope_table.h
+++ scope_table.h
@@ -3,147 +3,257 @@
 class scope_table
 {
 private:
-    int bucket_count;
-    int unique_id;
+    symbol_info** chains;
+    int tbl_size;
+    int num_chld = 0;
+    int ID;
     scope_table *parent_scope = NULL;
-    vector<list<symbol_info *>> table;
-
-    int hash_function(string name)
+    int hash_func(string symbol)
     {
-        unsigned int hash = 0;
-        for(char c : name)
+        int sum = 0;
+        for (int i = 0; i < symbol.size(); i++)
         {
-            hash = hash * 31 + c;
+            sum += (int)symbol[i];
         }
-        return hash % bucket_count;
+        return sum%tbl_size;
     }
-
 public:
-    scope_table();
-    scope_table(int bucket_count, int unique_id, scope_table *parent_scope);
-    scope_table *get_parent_scope();
-    int get_unique_id();
-    symbol_info *lookup_in_scope(symbol_info* symbol);
-    bool insert_in_scope(symbol_info* symbol);
-    bool delete_from_scope(symbol_info* symbol);
-    void print_scope_table(ofstream& outlog);
-    ~scope_table();
-};
-
-scope_table::scope_table()
-{
-    bucket_count = 10;
-    unique_id = 0;
-    parent_scope = NULL;
-    table.resize(bucket_count);
-}
-
-scope_table::scope_table(int bucket_count, int unique_id, scope_table *parent_scope)
-{
-    this->bucket_count = bucket_count;
-    this->unique_id = unique_id;
-    this->parent_scope = parent_scope;
-    table.resize(bucket_count);
-}
-
-scope_table* scope_table::get_parent_scope()
-{
-    return parent_scope;
-}
-
-int scope_table::get_unique_id()
-{
-    return unique_id;
-}
-
-symbol_info* scope_table::lookup_in_scope(symbol_info* symbol)
-{
-    int index = hash_function(symbol->get_name());
-    
-    for(symbol_info* sym : table[index])
+    scope_table(){}
+    scope_table(int n, int ID)
     {
-        if(sym->get_name() == symbol->get_name())
+        tbl_size = n;
+
+        chains = new symbol_info*[n];
+
+        for(int i = 0; i < n; i++)
         {
-            return sym;
+            chains[i] = NULL;
         }
+        this->ID = ID;
     }
-    return NULL;
-}
 
-bool scope_table::insert_in_scope(symbol_info* symbol)
-{
-    // Check if symbol already exists in this scope
-    if(lookup_in_scope(symbol) != NULL)
+    void set_prnt(scope_table *table)
     {
-        return false; // Symbol already exists
+        parent_scope = table;
+        if(parent_scope!=NULL) parent_scope->incrs_chld();
+        //set_scope_table_ID();
     }
-    
-    int index = hash_function(symbol->get_name());
-    table[index].push_back(symbol);
-    return true;
-}
 
-bool scope_table::delete_from_scope(symbol_info* symbol)
-{
-    int index = hash_function(symbol->get_name());
-    
-    for(auto it = table[index].begin(); it != table[index].end(); it++)
+    scope_table* get_prnt()
     {
-        if((*it)->get_name() == symbol->get_name())
+        return parent_scope;
+    }
+
+    // void set_scope_table_ID()
+    // {
+    //     if(parent_scope) ID = parent_scope->getID()+"."+to_string(parent_scope->get_num_chld());
+    //     else ID = "1";
+    // }
+
+    int get_num_chld()
+    {
+        return num_chld;
+    }
+
+    void incrs_chld()
+    {
+        num_chld++;
+    }
+
+    int getID()
+    {
+        return ID;
+    }
+
+    symbol_info* Lookup_in_scope(string name)
+    {
+        int pos=0;
+        int hash_val = hash_func(name);
+        symbol_info *curr_sym = chains[hash_val];
+
+        while(curr_sym != NULL)
         {
-            delete *it;
-            table[index].erase(it);
+            if (curr_sym->getname() == name)
+            {
+                return curr_sym;
+            }
+            else
+            {
+                pos++;
+                curr_sym = curr_sym->get_next();
+            }
+        }
+
+        return curr_sym;
+    }
+
+    bool Insert_in_scope(string name, string type)
+    {
+        int pos = 0;
+        symbol_info *new_sym = new symbol_info(name,type);
+
+        int hash_val = hash_func(name);
+
+        if(chains[hash_val]==NULL)
+        {
+            chains[hash_val] = new_sym;
             return true;
         }
-    }
-    return false;
-}
-
-void scope_table::print_scope_table(ofstream& outlog)
-{
-    outlog << "ScopeTable # " + to_string(unique_id) << endl;
-    
-    for(int i = 0; i < bucket_count; i++)
-    {
-        if(!table[i].empty())
+        else
         {
-            outlog << i << " --> " << endl;
-            for(symbol_info* sym : table[i])
+            if (chains[hash_val]->getname() == name)
             {
-                outlog << "< " << sym->get_name() << " : " << sym->get_type() << " >" << endl;
-                
-                if(sym->is_function())
+                return false;
+            }
+
+            pos++;
+            symbol_info *buffer = chains[hash_val];
+            symbol_info *curr_sym = chains[hash_val]->get_next();
+
+            while(true)
+            {
+                if(curr_sym == NULL)
                 {
-                    outlog << "Function Definition" << endl;
-                    outlog << "Return Type: " << sym->get_return_type() << endl;
-                    outlog << "Number of Parameters: " << sym->get_param_count() << endl;
-                    outlog << "Parameter Details: " << sym->get_param_details() << endl;
-                }
-                else if(sym->is_array())
-                {
-                    outlog << "Array" << endl;
-                    outlog << "Type: " << sym->get_data_type() << endl;
-                    outlog << "Size: " << sym->get_array_size() << endl;
+                    buffer->set_next(new_sym);
+                    return true;
                 }
                 else
                 {
-                    outlog << "Variable" << endl;
-                    outlog << "Type: " << sym->get_data_type() << endl;
+                    if (curr_sym->getname() == name)
+                    {
+                        return false;
+                    }
+                    pos++;
+                    buffer = curr_sym;
+                    curr_sym = curr_sym->get_next();
                 }
-                outlog << endl;
             }
         }
     }
-}
 
-scope_table::~scope_table()
-{
-    for(int i = 0; i < bucket_count; i++)
+    bool Delete_from_scope(string name)
     {
-        for(symbol_info* sym : table[i])
+        int pos = 0;
+        int hash_val = hash_func(name);
+        symbol_info *curr_sym = chains[hash_val];
+
+
+        if(curr_sym == NULL)
         {
-            delete sym;
+            return false;
         }
-        table[i].clear();
+
+        else if (curr_sym->getname() == name)
+        {
+            chains[hash_val] = curr_sym->get_next();
+            curr_sym->set_next(NULL);
+            delete curr_sym;
+            curr_sym = NULL;
+            return true;
+        }
+
+        else
+        {
+            pos++;
+            symbol_info *buffer = curr_sym;
+            curr_sym = curr_sym->get_next();
+            while(curr_sym!=NULL)
+            {
+                if (curr_sym->getname() == name)
+                {
+                    buffer->set_next(curr_sym->get_next());
+                    curr_sym->set_next(NULL);
+                    delete curr_sym;
+                    curr_sym = NULL;
+                    return true;
+                }
+                else
+                {
+                    pos++;
+                    buffer = curr_sym;
+                    curr_sym = curr_sym->get_next();
+                }
+            }
+            return false;
+        }
     }
-}
+
+    void Print_scope(ofstream& outlog)
+    {
+    	string s = "";
+    	s+="ScopeTable # "+to_string(ID)+"\n";
+        //cout<<"ScopeTable # "<<ID<<endl;
+
+        for(int i = 0; i < tbl_size; i++)
+        {
+            if(chains[i]!=NULL)
+            {
+            	s+=to_string(i)+" --> ";
+            	//cout<<i<<" --> ";
+
+		        symbol_info *curr_sym = chains[i];
+
+		        while(curr_sym!=NULL)
+		        {
+		        	s+="\n< "+curr_sym->getname()+" : "+curr_sym->gettype()+" >\n";
+                    if (curr_sym->getidtype() == "func_def")
+                    {
+                        s+="Function Definition\n";
+                        s+="Return Type: "+curr_sym->getvartype()+"\n";
+                        s+="Number of Parameters: "+to_string(curr_sym->getparamlist().size())+"\n";
+                        s+="Parameter Details: ";
+                        for(int i = 0; i<curr_sym->getparamlist().size(); i++)
+                        {
+                            s+=curr_sym->getparamlist()[i] + " " + curr_sym->getparamname()[i];
+                            if(i!=curr_sym->getparamlist().size()-1) s+=", ";
+                        }
+                        //cout<<"Function Definition"<<endl;
+                    }
+                    else if (curr_sym->getidtype() == "var")
+                    {
+                        s+="Variable\n";
+                        s+="Type: "+curr_sym->getvartype()+"\n";
+                        //cout<<"Variable"<<endl;
+                    }
+                    else if (curr_sym->getidtype() == "array")
+                    {
+                        s+="Array\n";
+                        s+="Type: "+curr_sym->getvartype()+"\n";
+                        s+="Size: "+to_string(curr_sym->getarraysize())+"\n";
+                        //cout<<"Array"<<endl;
+                    }
+                    else
+                    {
+                        s+="Error\n";
+                        //cout<<"Error"<<endl;
+                    }
+		            //cout<<"< "<<curr_sym->getname()<<" : "<<curr_sym->gettype()<<" > ";
+		            curr_sym = curr_sym->get_next();
+		        }
+				s+="\n";
+		        //cout<<endl;
+            }
+        }
+		s+="\n";
+		outlog<<s;
+        //cout<<endl;
+        //return s;
+    }
+
+    ~scope_table()
+    {
+        //cout<<"delete scope"<<endl;
+        for(int i = 0; i<tbl_size; i++)
+        {
+            while(chains[i]!=NULL)
+            {
+                symbol_info *buffer = chains[i];
+                chains[i] = chains[i]->get_next();
+                buffer->set_next(NULL);
+                delete buffer;
+                buffer = NULL;
+            }
+        }
+        delete[] chains;
+    }
+};
