3bd83fa3fd58b1aa15e295abb889689b52591fe6
Abrar Shahrier Arnab <120324766+arnob56@users.noreply.github.com>
2026-01-04 00:17:08 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+# First pass: Generate AST and symbol table
 yacc -d -y --debug --verbose 21201451.y
-echo 'Generated the parser C file as well the header file'
+echo 'Generated the parser C file and header file'
 g++ -w -c -o y.o y.tab.c
 echo 'Generated the parser object file'
 flex 21201451.l
 echo 'Generated the scanner C file'
 g++ -fpermissive -w -c -o l.o lex.yy.c
 echo 'Generated the scanner object file'
-g++ y.o l.o
-echo 'All ready, running'
-./a.exe input2.c
-echo 'logfile'
+g++ y.o l.o -o two_pass_compiler
+echo 'All ready, running the two-pass compiler...'
+
+# Run the compiler on the input file
+./two_pass_compiler input3.c
+echo 'Compilation completed.'
+
+# Display output files
+echo '------------ Log output ------------'
 cat 21201451_log.txt
+echo '------------ Error output ------------'
+cat 21201451_error.txt
+echo '------------ Three Address Code ------------'
+cat 21201451_code.txt
