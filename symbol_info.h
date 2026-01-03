bc74b2070b5e086cda2bde89e0efd130e013d05f
Abrar Shahrier Arnab <120324766+arnob56@users.noreply.github.com>
2026-01-04 00:17:43 +0600

    Update symbol_info.h

--- symbol_info.h
+++ symbol_info.h
@@ -1,169 +1,126 @@
-#include<bits/stdc++.h>
+#ifndef SYMBOL_INFO_H
+#define SYMBOL_INFO_H
+
+#include <bits/stdc++.h>
 using namespace std;
+
+// Forward declaration of ASTNode
+class ASTNode;
 
 class symbol_info
 {
 private:
-    string name;
-    string type;
-
-    // Additional attributes 
-    string symbol_type;  // variable/array/function
-    string data_type;    // int/float/void, etc.
-    int array_size;      // arrays
-    
-    // Function attributes
-    string return_type;
-    int param_count;
-    vector<string> param_types;
-    vector<string> param_names;
-
+    string sym_name;
+    string sym_type;
+    string ID_type; //var, array, func_dec, func_def
+    string var_type; //int, float, void, error
+    int array_size;
+    vector<string> param_list;//for functions
+    vector<string> param_name;
+    symbol_info *next_sym;
+    ASTNode* ast_node; // Pointer to AST node
 public:
+    //symbol_info(){}
     symbol_info(string name, string type)
     {
-        this->name = name;
-        this->type = type;
-        this->symbol_type = "";
-        this->data_type = "";
-        this->array_size = -1;
-        this->return_type = "";
-        this->param_count = 0;
+        sym_name = name;
+        sym_type = type;
+        next_sym = NULL;
+        ast_node = NULL;
     }
 
-    //Getter methods
-    string get_name()
+    void set_next(symbol_info *symbol)
     {
-        return name;
-    }
-    string get_type()
-    {
-        return type;
+        next_sym = symbol;
     }
 
-    // Symbol type methods
-    string get_symbol_type()
+    symbol_info* get_next()
     {
-        return symbol_type;
+        return next_sym;
     }
 
-    // Data type methods
-    string get_data_type()
+    string getname()
     {
-        return data_type;
+        return sym_name;
     }
-    // Array methods
-    int get_array_size()
+    string gettype()
+    {
+        return sym_type;
+    }
+    
+    string getvartype()
+    {
+        return var_type;
+    }
+    
+    void setvartype(string tp)
+    {
+    	var_type = tp;
+    }
+    
+    string getidtype()
+    {
+        return ID_type;
+    }
+    
+    void setidtype(string tp)
+    {
+    	ID_type = tp;
+    }
+    
+    int getarraysize()
     {
         return array_size;
     }
-
-    // Function methods
-    string get_return_type()
+    
+    void setarraysize(int sz)
     {
-        return return_type;
-    }
-    int get_param_count()
-    {
-        return param_count;
-    }
-
-    vector<string> get_param_types()
-    {
-        return param_types;
+    	array_size = sz;
     }
     
-    vector<string> get_param_names()
+    void setparamlist(vector<string> list)
     {
-        return param_names;
+    	param_list = list;
     }
     
-    string get_param_details()
+    vector<string> getparamlist()
     {
-        string details = "";
-        for(int i = 0; i < param_count; i++)
-        {
-            if(i > 0) details += ", ";
-            details += param_types[i];
-            if(!param_names[i].empty())
-                details += " " + param_names[i];
-        }
-        return details;
-    }
-
-    // For backward compatibility - some grammar rules use getname()
-    string getname()
-    {
-        return name;
-    }
-
-
-    //Setter methods
-    void set_name(string name)
-    {
-        this->name = name;
-    }
-    void set_type(string type)
-    {
-        this->type = type;
-    }
-    // Symbol type methods
-    void set_symbol_type(string symbol_type)
-    {
-        this->symbol_type = symbol_type;
-    }
-    // Data type methods
-    void set_data_type(string data_type)
-    {
-        this->data_type = data_type;
-    }
-     
-    // Array methods
-    void set_array_size(int size)
-    {
-        this->array_size = size;
-        this->symbol_type = "array";
-    }
-    bool is_array()
-    {
-        return array_size != -1;
-    }
-    // Function methods
-    void set_return_type(string return_type)
-    {
-        this->return_type = return_type;
-        this->symbol_type = "function";
-    }
-    void set_param_count(int count)
-    {
-        this->param_count = count;
-    }
-    void add_parameter(string type, string name)
-    {
-        param_types.push_back(type);
-        param_names.push_back(name);
-        param_count++;
-    }
-
-    bool is_function()
-    {
-        return symbol_type == "function";
+    	return param_list;
     }
     
+    vector<string> getparamname()
+    {
+    	return param_name;
+    }
+    
+    void setparamname(vector<string> list)
+    {
+    	param_name = list;
+    }
+    
+    int getparamsize()
+    {
+    	return param_list.size();
+    }
+
+    // New methods for AST support
+    void set_ast_node(ASTNode* node)
+    {
+        ast_node = node;
+    }
+
+    ASTNode* get_ast_node()
+    {
+        return ast_node;
+    }
+
     ~symbol_info()
     {
-        // Explicitly clear containers and strings to release memory early.
-        param_types.clear();
-        param_types.shrink_to_fit();
-        param_names.clear();
-        param_names.shrink_to_fit();
-
-        name.clear();
-        type.clear();
-        symbol_type.clear();
-        data_type.clear();
-        return_type.clear();
-
-        array_size = -1;
-        param_count = 0;
+        delete next_sym;
+        param_list.clear();
+        param_name.clear();
+        // Don't delete ast_node here - will be managed separately
     }
 };
+
+#endif // SYMBOL_INFO_H
